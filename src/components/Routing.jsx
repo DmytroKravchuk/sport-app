3b833e8614939010e3809c52ee4405e289110b2c
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2020-05-17 00:53:57 +0300

    update(*): routing, create empty Play component

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -3,6 +3,7 @@ import {Route, Switch} from "react-router-dom";
 import StartPage from '../components/StartPage';
 import Programs from "./Programs";
 import ProgramsDays from "./ProgramsDays";
+import Play from "./Play";
 
 const Routing = () => {
     return (
@@ -10,11 +11,12 @@ const Routing = () => {
             <Switch>
                 <Route exact path='/' component={StartPage}/>
                 <Route exact path='/pushups' component={() => <Programs typeProgram='pushups' currentPath='pushups'/>}/>
-                <Route path='/pushups/:id' component={() => <ProgramsDays path='/pushups'/>}/>
+                <Route path='/pushups/:id' component={() => <ProgramsDays path='/pushups' pathToPlay='/play'/>}/>
                 <Route exact path='/pullups' component={() => <Programs typeProgram='pullups' currentPath='pullups'/>}/>
-                <Route path='/pullups/:id' component={() => <ProgramsDays path='/pullups'/>}/>
+                <Route path='/pullups/:id' component={() => <ProgramsDays path='/pullups' pathToPlay='/play'/>}/>
                 <Route exact path='/squats' component={() => <Programs typeProgram='squats' currentPath='squats'/>}/>
-                <Route path='/squats/:id' component={() => <ProgramsDays path='/squats'/>}/>
+                <Route path='/squats/:id' component={() => <ProgramsDays path='/squats' pathToPlay='/play'/>}/>
+                <Route path='/play' component={() => <Play/>}/>
                 <Route path='*' component={() => <div>Error</div>}/>
             </Switch>
         </>
